da2104a5b426742a1c8d93e6e4656a128d727fd3
Abdel593 <abd.benabou@gmail.com>
2024-01-27 17:30:53 +0100

    commit 7

--- src/main/java/com/example/demo/SpringBootJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringBootJenkinsApplication.java
@@ -8,7 +8,7 @@ public class SpringBootJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
-		System.out.println("test 7");
+		System.out.println("test 8");
 	}
 
 }
